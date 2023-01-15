4e53c1ca623156c47db35cbd4476b53aa5b60b38
synthetic-borealis <snip.coloured@gmail.com>
2023-01-15 17:11:39 +0200

    add openapi/swagger

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::http::Status;
+use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
 
+#[openapi]
+#[get("/<url_id>")]
+fn redirect_url(url_id: String) -> Result<String, Status> {
+    Ok(format!("Hello, {}", url_id))
+}
+
+#[openapi]
 #[get("/")]
-fn hello() -> Result<String, Status> {
+fn home() -> Result<String, Status> {
     Ok("Hello, World!".to_string())
 }
 
 #[launch]
 fn rocket() -> rocket::Rocket<rocket::Build> {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .mount("/", openapi_get_routes![home, redirect_url])
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
 }
-

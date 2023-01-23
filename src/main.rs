9ae568ef9e3c7688db16a97d4d2c702769e77f92
synthetic-borealis <snip.coloured@gmail.com>
2023-01-23 17:52:10 +0200

    enable static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,46 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::http::Status;
-use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
+use std::net::Ipv4Addr;
+use std::path::{Path, PathBuf};
 
-#[openapi]
+use rocket::Config;
+use rocket::fs::{NamedFile, relative};
+use rocket::http::Status;
+use rocket::response::Redirect;
+
+#[get("/app/<_..>")]
+async fn app() -> Option<NamedFile> {
+    NamedFile::open("static/index.html").await.ok()
+}
+
+#[get("/static/<path..>")]
+async fn static_dir(path: PathBuf) -> Option<NamedFile> {
+    let mut path = Path::new(relative!("static")).join(path);
+
+    if path.is_dir() {
+        path.push("index.html");
+    }
+
+    NamedFile::open(path).await.ok()
+}
+
 #[get("/<url_id>")]
 fn redirect_url(url_id: String) -> Result<String, Status> {
     Ok(format!("Hello, {}", url_id))
 }
 
-#[openapi]
 #[get("/")]
-fn home() -> Result<String, Status> {
-    Ok("Hello, World!".to_string())
+fn home() -> Redirect {
+    Redirect::to("/app")
 }
 
 #[launch]
 fn rocket() -> rocket::Rocket<rocket::Build> {
-    rocket::build()
-        .mount("/", openapi_get_routes![home, redirect_url])
-        .mount(
-            "/swagger-ui/",
-            make_swagger_ui(&SwaggerUIConfig {
-                url: "../openapi.json".to_owned(),
-                ..Default::default()
-            }),
-        )
+    let config = Config {
+        address: Ipv4Addr::new(0, 0, 0, 0).into(),
+        ..Config::default()
+    };
+    rocket::custom(&config)
+        .mount("/", routes![home, app, static_dir, redirect_url])
 }
